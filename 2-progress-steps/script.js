a66234f15c96e11f978ee8ad8c3f306634fa65fc
Caio Mizumoto <caio.mizumoto@gmail.com>
2022-09-14 23:33:59 -0300

    update read me, add progress steps project

---
+++ 2-progress-steps/script.js
@@ -0,0 +1,44 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const squares = document.querySelectorAll(".square");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > squares.length) {
+    currentActive = squares.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+const update = () => {
+  squares.forEach((square, index) => {
+    if (index < currentActive) {
+      square.classList.add("active");
+    } else {
+      square.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width = ((actives.length - 1) / (squares.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === squares.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+};
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Small Projects
+# Small Projects
 
 This is a series of small projects using HTML, CSS and JS.
 Mostly small components, image slide shows, small games, fetching from APIs.
@@ -6,4 +6,5 @@ Also uploading them in netlify.
 
 ### Projects
 
-1- Expanding Cards (live version soon)
+1- Expanding Cards
+2- Progress Bar Steps
